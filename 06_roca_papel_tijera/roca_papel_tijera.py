516044970ff489b0e69bb7cd53dd9130a12c2b4e
jdavilasoft <jdavila.soft@gmail.com>
2022-11-24 18:22:43 -0500

    Proyecto 06

--- 06_roca_papel_tijera/roca_papel_tijera.py
+++ 06_roca_papel_tijera/roca_papel_tijera.py
@@ -1,16 +1,12 @@
 import random
 
-juego = ["ROCA", "PAPEL", "TIJERA"]
+game = ["ROCK", "PAPER", "SCISSORS"]
 
 play1, play2 = random.randint(0, 2), random.randint(0, 2)
 
 if play1 == play2:
-    print("EMPATE [ Jugador 1:", juego[play1], "Jugador 2:", juego[play2], "]")
-elif play1 - play2 == 1:
-    print("GANA Jugador 1:", juego[play1], "Jugador 2:", juego[play2], "]")
-elif play2 - play1 == 1:
-    print("GANA Jugador 2:", juego[play2], "Jugador 1:", juego[play1], "]")
-elif play1 - play2 == 2:
-    print("GANA Jugador 2:", juego[play2], "Jugador 1:", juego[play1], "]")
-elif play2 - play1 == 2:
-    print("GANA Jugador 1:", juego[play1], "Jugador 2:", juego[play2], "]")
+    print("DRAW [ Player 1:", game[play1], "= Player 2:", game[play2], "]")
+elif play1 - play2 == 1 or play2 - play1 == 2:
+    print("WIN Player 1:", game[play1], "Player 2:", game[play2])
+elif play2 - play1 == 1 or play1 - play2 == 2:
+    print("WIN Player 2:", game[play2], "Player 1:", game[play1])
