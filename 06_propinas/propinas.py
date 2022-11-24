cf2424801aac7626818c3a4a0df53d6ccfd7ccc2
jdavilasoft <jdavila.soft@gmail.com>
2022-11-24 16:29:08 -0500

    Proyecto 06

---
+++ 06_propinas/propinas.py
@@ -0,0 +1,5 @@
+total_fac = float(input("Ingresa el total de la factura: "))
+
+propina = total_fac * 0.18
+
+print("La propina es", round(propina, 2), ", el total general es:", round(total_fac + propina, 2))
