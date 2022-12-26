f9ec098cea53f5b486b889f861e27e6fc14eb0d9
jdavilasoft <jdavila.soft@gmail.com>
2022-12-26 11:03:17 -0500

    Proyecto 04

---
+++ 04_biografia/biografia.py
@@ -0,0 +1,10 @@
+
+nombre = input('Nombre completo: ')
+edad = input('Fecha de nacimiento: ')
+metas23 = input('Metas para el anio 2023: ')
+
+print(
+    "- Nombres: ", nombre, "\n"
+    "- Edad: ", edad, "\n"
+    "- Metas anio nuevo: ", metas23
+)
